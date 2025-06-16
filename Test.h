3df62f281ba0ec92cc32f24b02bfb3963ac15a0e
anEggYolk <165854696+anEggYolk@users.noreply.github.com>
2025-06-16 12:26:32 +1000

    Create Test.h

---
+++ Test.h
@@ -0,0 +1,17 @@
+// Created by lucas on 12/06/2025
+
+#ifndef TESTS_H
+#define TESTS_H
+
+#include <cstdint>
+
+void printTestBoard(uint64_t wPawnBit, uint64_t wKnightBit, uint64_t wBishopBit,
+                    uint64_t wRookBit, uint64_t wQueenBit, uint64_t wKingBit,
+                    uint64_t bPawnBit, uint64_t bKnightBit, uint64_t bBishopBit,
+                    uint64_t bRookBit, uint64_t bQueenBit, uint64_t bKingBit);
+
+class Tests {
+    // Add members or methods here if needed later
+};
+
+#endif // TESTS_H
